971fa9865ded411883effe72eb7da10401e57de8
Nicholas Singh <singhnicholas.ext@gmail.com>
2020-08-06 11:39:08 -0700

    final

---
+++ Practice/Python_Practice.py
@@ -0,0 +1,3 @@
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
